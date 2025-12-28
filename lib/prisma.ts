7fe1454b6a877d6f9bf722620771cac61c49dd80
v-team-v <mail.kiselevv@gmail.com>
2025-12-28 21:13:35 +0300

    chore(debug): add prisma query duration logging

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,24 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
+const prismaClientSingleton = () => {
+  const client = new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+    ],
   });
+
+  client.$on('query', (e) => {
+    console.log('Query: ' + e.query);
+    console.log('Duration: ' + e.duration + 'ms');
+  });
+
+  return client;
+};
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
