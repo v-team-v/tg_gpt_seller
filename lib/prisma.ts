ec763b34191f911c3a98ef8814e723bc4dad74ef
v-team-v <mail.kiselevv@gmail.com>
2025-12-28 22:01:03 +0300

    chore: revert prisma logging

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,21 +3,8 @@ import { PrismaClient } from '@prisma/client';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prismaClientSingleton = () => {
-  const client = new PrismaClient({
-    log: [
-      {
-        emit: 'event',
-        level: 'query',
-      },
-    ],
-  });
-
-  client.$on('query', (e) => {
-    console.log('Query: ' + e.query);
-    console.log('Duration: ' + e.duration + 'ms');
-  });
-
-  return client;
+  return new PrismaClient()
+}
 };
 
 export const prisma = globalForPrisma.prisma || prismaClientSingleton();
